04097d5eb358983cdc393ee2ebabbc96997df892
ebook-fujimoto <r.fujimoto@ebookjapan.co.jp>
2020-05-16 02:57:03 +0000

    add voucher-admin

--- setup_git.sh
+++ setup_git.sh
@@ -8,6 +8,7 @@ setup() {
     ghq get -p git@github.com:ebook-japan/Infra-AWS-Service-AMI.git
     ghq get -p git@github.com:ebook-japan/Infra-AWS-Service.git
     ghq get -p git@github.com:ebook-japan/no1api-voucher.git
+    ghq get -p git@github.com:ebook-japan/no1api-voucher-admin.git
     ghq get -p git@github.com:ebook-japan/agency-batch.git
 }
 

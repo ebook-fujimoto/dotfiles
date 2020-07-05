1d1003d8c9f9f4b2327359f305194e79f3aa725c
ebook-fujimoto <r.fujimoto@ebookjapan.co.jp>
2020-07-05 11:15:23 +0000

    mod readme, and git repo

--- README.md
+++ README.md
@@ -5,3 +5,20 @@ curl -sL https://raw.githubusercontent.com/ebook-fujimoto/dotfiles/master/setup.
 ```
 
 ref: https://orgachem.hatenablog.com/entry/2014/05/13/001100
+
+## setup
+
+- ssh
+
+```
+sh setup_ssh.sh
+```
+
+go to https://github.com/settings/keys
+
+- go and git
+
+```
+sh setup_go.sh
+sh setup_git.sh
+```
--- setup_git.sh
+++ setup_git.sh
@@ -9,6 +9,9 @@ setup() {
     ghq get -p git@github.com:ebook-japan/Infra-AWS-Service.git
     ghq get -p git@github.com:ebook-japan/no1api-voucher.git
     ghq get -p git@github.com:ebook-japan/no1api-voucher-admin.git
+    ghq get -p git@github.com:ebook-japan/coin-batch.git
+    ghq get -p git@github.com:ebook-japan/coupon-batch.git
+    ghq get -p git@github.com:ebook-japan/ticket-batch.git
     ghq get -p git@github.com:ebook-japan/agency-batch.git
 }
 

aec515cc63f8e274d43a8b015089893fc99cc272
ebook-fujimoto <r.fujimoto@ebookjapan.co.jp>
2020-12-24 06:07:52 +0000

    add Infra-AWS-office

--- setup_git.sh
+++ setup_git.sh
@@ -7,6 +7,7 @@ setup() {
     ghq get -p git@github.com:ebook-japan/no1api-devops.git
     ghq get -p git@github.com:ebook-japan/Infra-AWS-Service-AMI.git
     ghq get -p git@github.com:ebook-japan/Infra-AWS-Service.git
+    ghq get -p git@github.com:ebook-japan/Infra-AWS-office.git
     ghq get -p git@github.com:ebook-japan/no1api-voucher.git
     ghq get -p git@github.com:ebook-japan/no1api-voucher-admin.git
     ghq get -p git@github.com:ebook-japan/coin-batch.git

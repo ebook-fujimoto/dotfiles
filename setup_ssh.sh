2da5ca85ea0732554ec3ca99a01145fdaf4841af
ebook-fujimoto <r.fujimoto@ebookjapan.co.jp>
2021-03-22 02:37:52 +0900

    ed25519

--- setup_ssh.sh
+++ setup_ssh.sh
@@ -1,6 +1,7 @@
 
-KEY_NAME=`whoami`_on_`date --iso-8601="seconds"`
-ssh-keygen -C $KEY_NAME -f $HOME/.ssh/id_rsa -N "" -b 4092
-cat $HOME/.ssh/id_rsa.pub
+KEY_NAME=`whoami`_at_`hostname`_on_`date --iso-8601="seconds"`
+TYPE=ed25519
+ssh-keygen -t $TYPE -C $KEY_NAME -f $HOME/.ssh/id_$TYPE -N ""
+cat $HOME/.ssh/id_$TYPE.pub
 
 echo "go to https://github.com/settings/keys"
